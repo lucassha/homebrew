d8040215eea6531393fa0c9839a38d8f7f7d9705
Shannon Lucas <shannonlucas4@gmail.com>
2021-07-29 10:04:05 -0700

    clean up formula

--- Formula/show-secrets.rb
+++ Formula/show-secrets.rb
@@ -7,17 +7,14 @@ class ShowSecrets < Formula
     # find latest release: aws s3api list-objects --bucket lucassha-show-secrets-releases | jq '.Contents[].Key'
     # download release: aws s3 cp s3://lucassha-show-secrets-releases/releases/v0.0.1 . 
     # get sha: shasum -a 256 v0.0.1
-    # 
-
     sha256 "36018cff6708dea3587c086e366ff80bed7a2750ddda9aa7e8c1af8311b17649"
 
     depends_on "kubernetes-cli"
 
     def install
         bin.install "show-secrets"
-        bin.install "kubectl-show-secrets"
         # add kubectl binary to make it a kubectl plugin
-        # cp bin/"show-secrets" bin/"kubectl-show-secrets"
+        bin.install "kubectl-show-secrets"
     end
 
     def caveats; <<-EOS
